9b189591239dde9304d0a82ae438077c318ba05a
Ankur Oberoi <aoberoi@gmail.com>
2019-09-30 13:44:05 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const { App } = require('@slack/bolt');
+
+// Initialize your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.event('app_home_opened', ({ say }) => {
+  say('Hello, world!');
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
